d0ac2a45ff818933ab530d329267cdf2902d73d2
Raj Shah <rajmshah17@gmail.com>
2023-05-01 19:52:00 +0530

    progress on home

---
+++ static/js/text-injector.js
@@ -0,0 +1,7 @@
+import data from './../data/text.json' assert { type: 'json' };
+
+var intro_title = data.intro.title
+var intro_text = data.intro.text
+
+document.getElementById('intro_title').innerHTML = intro_title
+document.getElementById('intro_text').innerHTML = intro_text
