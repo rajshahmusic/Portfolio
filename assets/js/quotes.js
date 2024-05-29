8ba0de1758bc04da35670d11bca369fb8a32be15
Jaivanth <94882495+Not-A-Psychic@users.noreply.github.com>
2024-05-29 08:12:27 +0000

    text-injector test

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,34 +1,14 @@
-// import data from "{{ '/assets/data/quotes.json' | relative_url }}" assert { type: 'json' };
+import data from "./data/quotes.json" assert { type: 'json' };
 
 
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
-// var i = getRandomInt(358);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+var i = getRandomInt(358);
 
-// var quote = data.quotes[i].quote;
-// var author = data.quotes[i].author;
+var quote = data.quotes[i].quote;
+var author = data.quotes[i].author;
 
 
-// document.getElementById("quote").innerHTML = '"' + quote + '"';
-// document.getElementById("author").innerHTML = '-' + author;
-
-document.addEventListener("DOMContentLoaded", function() {
-  fetch("{{ '/assets/data/quotes.json' | relative_url }}")
-    .then(response => response.json())
-    .then(data => {
-      function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-      }
-      var i = getRandomInt(data.quotes.length);
-
-      var quote = data.quotes[i].quote;
-      var author = data.quotes[i].author;
-
-      document.getElementById("quote").innerHTML = '"' + quote + '"';
-      document.getElementById("author").innerHTML = '-' + author;
-    })
-    .catch(error => {
-      console.error('Error fetching quotes:', error);
-    });
-});
+document.getElementById("quote").innerHTML = '"' + quote + '"';
+document.getElementById("author").innerHTML = '-' + author;
